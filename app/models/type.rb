873f336905de5a77842bf3d5e8c8fedd3a19ff4b
Josh Brown <joshbrown15@gmail.com>
2014-08-11 17:02:34 +0000

    WIP: Create proper type selector

--- app/models/type.rb
+++ app/models/type.rb
@@ -1,6 +1,14 @@
 class Type < ActiveRecord::Base
   has_many :products
 
+  def nice_name
+    self.name.gsub('_', ' ').gsub(' and ', ' & ').titlecase
+  end
+
+  def link
+    [self.nice_name, '/!' + self.code]
+  end
+
   before_save :underscores_to_spaces!
 
   protected
